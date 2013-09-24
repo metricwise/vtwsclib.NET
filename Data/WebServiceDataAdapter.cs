f053d7edaac7e5c7c2a50613f832c21b6ddf9a81
Adam Heinz <amh@metricwise.net>
2013-09-24 11:59:23 -0700

    WARESCAN-49 #comment Added Fill method to web service data adapter.

--- Data/WebServiceDataAdapter.cs
+++ Data/WebServiceDataAdapter.cs
@@ -4,19 +4,52 @@ using System.Data.Common;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
+using LastModifiedDictionary = System.Collections.Generic.Dictionary<string, System.DateTime>;
 
 namespace Vtiger.Data
 {
     public class WebServiceDataAdapter : DataAdapter
     {
+        public LastModifiedDictionary lastModifiedDict = new LastModifiedDictionary();
         private WebServiceClient webServiceClient = null;
 
         public WebServiceDataAdapter(WebServiceClient client)
         {
             webServiceClient = client;
+        }
+
+        public override int Fill(DataSet dataSet)
+        {
+            int filled = 0;
+            foreach (DataTable table in dataSet.Tables)
+            {
+                DateTime lastModified;
+                if (lastModifiedDict.ContainsKey(table.TableName))
+                {
+                    lastModified = lastModifiedDict[table.TableName];
+                }
+                else
+                {
+                    lastModified = DateTime.Now - TimeSpan.FromDays(90); // TODO: 90 days is arbitrary
+                }
+                int modifiedTime = ToUnixTime(lastModified);
+                JToken token = webServiceClient.DoSync(modifiedTime.ToString(), table.TableName);
+                if (token != null)
+                {
+                    DataTable dataTable = ToDataTable(token["updated"]);
+                    DataTable mergeTable = dataSet.Tables[table.TableName].Clone();
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        mergeTable.ImportRow(row);
+                    }
+                    dataSet.Merge(mergeTable);
+                }
+            }
+            return filled;
         }
 
         public override int Update(DataSet dataSet)
@@ -42,14 +75,52 @@ namespace Vtiger.Data
         {
         }
 
-        private JToken ToToken(DataRow value)
+        private DataTable ToDataTable(JToken token)
+        {
+            if (!(token is JArray))
+            {
+                JArray container = new JArray();
+                container.Add(token);
+                token = container;
+            }
+            JTokenReader reader = new JTokenReader(token);
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new DataTableConverter());
+            DataTable dataTable = serializer.Deserialize<DataTable>(reader);
+            if (!dataTable.Columns.Contains("crmid"))
+            {
+                dataTable.Columns.Add("crmid", typeof(int));
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    row["crmid"] = ToCrmId(row["id"].ToString());
+                }
+                dataTable.PrimaryKey = new DataColumn[] { dataTable.Columns["crmid"] };
+            }
+            return dataTable;
+        }
+
+        private int ToCrmId(string wsId)
+        {
+            int crmId = int.Parse(Regex.Replace(wsId, ".*x", ""));
+            return crmId;
+        }
+
+        private JToken ToToken(DataRow row)
         {
             JTokenWriter writer = new JTokenWriter();
             JsonSerializer serializer = new JsonSerializer();
             serializer.Converters.Add(new DataTableConverter());
-            serializer.Serialize(writer, value);
+            serializer.Serialize(writer, row);
             JToken token = writer.Token["Table"][0];
             return token;
+        }
+
+        private int ToUnixTime(DateTime dateTime)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+            TimeSpan span = dateTime - epoch;
+            int unixtime = (int)Math.Round(span.TotalSeconds);
+            return unixtime;
         }
     }
 }
